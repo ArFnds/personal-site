117c891317c6591aca034c8654a6fafedc71a81c
Arnaud Fernandes <arnaud.fernandes@outlook.fr>
2025-01-08 13:35:45 +0100

    feat: more prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,18 @@
 import type { Config } from "@react-router/dev/config";
+import { availableLanguages } from "./app/i18n/i18n";
+
+const prerender = availableLanguages.flatMap((lang) => [
+	`/${lang}`,
+	`/${lang}/about`,
+	`/${lang}/portfolio`,
+	`/${lang}/mentoring`,
+	`/${lang}/conferences`,
+	`/${lang}/contact`,
+]);
 
 export default {
 	// Config options...
 	// Server-side render by default, to enable SPA mode set this to `false`
 	ssr: true,
-	prerender(args) {
-		return ["/fr", "/en", "/ru"];
-	},
+	prerender,
 } satisfies Config;
