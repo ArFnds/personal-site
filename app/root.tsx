783a69fe99386126ea445a16f13de49d2a72de6d
Arnaud Fernandes <arnaud.fernandes@outlook.fr>
2025-01-08 07:15:09 +0100

    refactor: move script

--- app/root.tsx
+++ app/root.tsx
@@ -70,8 +70,38 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<Meta />
 				<Links />
+				{/* Yandex.Metrika counter */}
+				<script
+					type="text/javascript"
+					// biome-ignore lint/security/noDangerouslySetInnerHtml: Yandex.Metrika
+					dangerouslySetInnerHTML={{
+						__html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(99440606, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });`,
+					}}
+				/>
+				{/* /Yandex.Metrika counter */}
 			</head>
 			<body>
+				{/* Yandex.Metrika counter */}
+				<noscript>
+					<div>
+						<img
+							src="https://mc.yandex.ru/watch/99440606"
+							style={{ position: "absolute", left: "-9999px" }}
+							alt=""
+						/>
+					</div>
+				</noscript>
+				{/* /Yandex.Metrika counter */}
 				<div className="min-h-screen bg-background text-foreground">
 					<I18nextProvider i18n={i18n}>
 						<ThemeProvider defaultTheme="system">
@@ -97,34 +127,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				gtag('config', 'G-9X1ZS89XE1');`,
 					}}
 				/>
-				{/* Yandex.Metrika counter */}
-				<script
-					type="text/javascript"
-					// biome-ignore lint/security/noDangerouslySetInnerHtml: Yandex.Metrika
-					dangerouslySetInnerHTML={{
-						__html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(99440606, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });`,
-					}}
-				/>
-				<noscript>
-					<div>
-						<img
-							src="https://mc.yandex.ru/watch/99440606"
-							style={{ position: "absolute", left: "-9999px" }}
-							alt=""
-						/>
-					</div>
-				</noscript>
-				{/* /Yandex.Metrika counter */}
 			</body>
 		</html>
 	);
